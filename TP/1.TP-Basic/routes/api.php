1b5534ca4e9acb7d376284942a3e6d1a0dd9bf0b
nada stitou <nadastito1087@gmail.com>
2022-11-16 21:39:56 +0100

    route

--- TP/1.TP-Basic/routes/api.php
+++ TP/1.TP-Basic/routes/api.php
@@ -22,4 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::get('/tache',[tacheController::class,'index']);
 Route::post('/store',[tacheController::class,"store"]);
+Route::post('/update/{id}',[tacheController::class,"update"]);
 Route::delete('/delete/{id}',[tacheController::class,"destroy"]);
