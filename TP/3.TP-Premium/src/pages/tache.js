5b6b8648a67bbafd479584fac2d6391789b74e9c
stitou-nada <nadastito1087@gmail.com>
2022-11-21 16:59:35 +0100

    tp premium

--- TP/3.TP-Premium/src/pages/tache.js
+++ TP/3.TP-Premium/src/pages/tache.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import "style.css";
 class Tache extends React.Component{
 
     state={
@@ -44,11 +44,10 @@ class Tache extends React.Component{
        await axios.get("http://127.0.0.1:8000/api/edit/"+id)
         .then(response=>{
             this.setState({
-                nom:response.data.Nom,
+                nom:response.data.nom,
                 id:response.data.id
             })
         })
-        
     }
     handleUpdate= async(e)=>{
          e.preventDefault()
@@ -63,21 +62,28 @@ class Tache extends React.Component{
 
     render(){
         return(
-            <div>
-            <form>
-                     Ajouter nom de tache <input value={this.state.nom} onChange={this.handleChange}></input>
+            <section className="vh-100" style={{backgroundColor: '#eee'}}>
+            <div className="container py-5 h-100">
+              <div className="row d-flex justify-content-center align-items-center h-100">
+                <div className="col col-lg-9 col-xl-7">
+                  <div className="card rounded-3">
+                    <div className="card-body p-4">
+                      <form className="row row-cols-lg-auto g-3 justify-content-center align-items-center mb-4 pb-2">
+                        <div className="col-12">
+                      <input value={this.state.nom} onChange={this.handleChange} placeholder="Enter nom de tache"></input>
                     <button onClick={this.handleClick} className="btn btn-primary">Add</button>
                     {/* <button onClick={this.handleClick} className="btn btn-primary">up</button> */}
                     <button onClick={this.handleUpdate} className="btn btn-success">Update</button>
+                    </div>
 
             </form>
             
-        <table>
+        <table className="table mb-4">
             <thead>
                 <tr>
-                <th>Id</th>
-                <th>Nom</th>
-                <th>Action</th>
+                <th scope="col">Id</th>
+                <th scope="col">Nom</th>
+                <th scope="col">Action</th>
                 </tr>
             </thead>
             <tbody>
@@ -85,9 +91,9 @@ class Tache extends React.Component{
 
                 
                 <tr key={value.id}>
-                    <td>{value.id}</td>
+                    <td scope="row">{value.id}</td>
                     <td>{value.Nom}</td>
-                    <td>
+                    <td style={{width: '216px'}}>
                         <button class='btn btn-danger' onClick={()=>this.handleDelete(value.id)}>suprimer</button>
                         <button class='btn btn-success' onClick={()=>this.handleEdit(value.id)}>Modifier</button>
                     </td>
@@ -96,7 +102,11 @@ class Tache extends React.Component{
             </tbody>
         </table>
         </div>
-        
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
         )
     }
 }
