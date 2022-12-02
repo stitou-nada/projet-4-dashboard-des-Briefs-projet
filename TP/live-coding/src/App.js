1ed6df186f5cd77e64a2f0b4084c5f80d8590ae2
stitou-nada <nadastito1087@gmail.com>
2022-12-02 09:24:30 +0100

    chart js

--- TP/live-coding/src/App.js
+++ TP/live-coding/src/App.js
@@ -1,12 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import Tache from './pages/tache';
-function App() {
+import ChartBar from './pages/chartjs';
+import axios from 'axios';
+import React from 'react';
+class App extends React.Component {
+  state={
+    data:[],
+    nom:'',
+    id:''
+}
+
+componentDidMount(){
+    axios.get("http://127.0.0.1:8000/api/tache")
+    .then(res=>{
+        this.setState({
+            data:res.data
+        })
+    })
+}
+  render() {
   return (
     <div className="App">
+      
       <Tache />
+     <ChartBar  dataProps={this.state.data} />
     </div>
   );
 }
+}
+
 
 export default App;
