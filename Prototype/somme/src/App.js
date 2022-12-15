712d6c8a9d159386ff1706390efb2b977a1d0e19
stitou-nada <nadastito1087@gmail.com>
2022-12-15 14:08:19 +0100

    debug de a+b

---
+++ Prototype/somme/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  let a=2
+  let b=4
+  let Z=a+b
+  return (
+    <div className="App">
+     <h1>La somme</h1>
+     <p>resultat de A + B ={Z}</p>
+    </div>
+  );
+}
+
+export default App;
