b1902ac1ec0317a28131e09bb52841cbb16cae4c
stitou-nada <nadastito1087@gmail.com>
2022-12-15 12:10:43 +0100

    get cookies

--- Realisation/front-end/src/component/dashbord/dashbord.jsx
+++ Realisation/front-end/src/component/dashbord/dashbord.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import Cookies from "universal-cookie/cjs/Cookies";
+
 
 class Dashbord extends React.Component{
    
 render(){
+    const cookies = new Cookies()
+
     return(
         <div>
           <h1> hello nada a dashbord</h1> 
+          {cookies.get('idFormateur')}
         </div>
     )
 }
--- Realisation/front-end/src/component/login/login.jsx
+++ Realisation/front-end/src/component/login/login.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { redirect, useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
 function Login (){
 
  const navigate= useNavigate()
+ const cookies = new Cookies()
 
     const[Data, setData] = useState([])
     const[Nom, setNom] = useState([])
     const[Email, setEmail] = useState([])
-
     useEffect(()=>{
         
            axios.get("http://127.0.0.1:8000/api/formateur")
@@ -34,10 +35,13 @@ function Login (){
            let data = Data
            let nom = Nom
            let email = Email
+
            data.map((value)=>{
 
              if (nom == value.Nom_formateur && email==value.Email_formateur) {
+                cookies.set('idFormateur',value.id)
                 return navigate("/Dashbord")
+
               }
       
        
