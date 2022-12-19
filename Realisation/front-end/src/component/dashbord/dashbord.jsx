8e8249871cd047d2164906841646b36e3bcc2a57
stitou-nada <nadastito1087@gmail.com>
2022-12-19 09:31:02 +0100

    add

--- Realisation/front-end/src/component/dashbord/dashbord.jsx
+++ Realisation/front-end/src/component/dashbord/dashbord.jsx
@@ -39,72 +39,6 @@ render(){
     return(
          
         <div>
-            <div>
-            ;<div className="wrapper d-flex align-items-stretch">
-  <nav id="sidebar" className="active">
-    <div className="custom-menu">
-      <button type="button" id="sidebarCollapse" className="btn btn-primary">
-        <i className="fa fa-bars" />
-        <span className="sr-only">Toggle Menu</span>
-      </button>
-    </div>
-    <div className="p-4">
-      <h1>
-        <a href="index.html" className="logo">
-          Flash
-        </a>
-      </h1>
-      <ul className="list-unstyled components mb-5">
-        <li className="active">
-          <a href="#">
-            <span className="fa fa-home mr-3" /> Home
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <span className="fa fa-user mr-3" /> About
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <span className="fa fa-briefcase mr-3" /> Portfolio
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <span className="fa fa-sticky-note mr-3" /> Blog
-          </a>
-        </li>
-        <li>
-          <a href="#">
-            <span className="fa fa-paper-plane mr-3" /> Contact
-          </a>
-        </li>
-      </ul>
-      
-      <div className="footer">
-        <p>
-          {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
-          Copyright © All rights reserved | This template is made with{" "}
-          <i className="icon-heart" aria-hidden="true" /> by{" "}
-          <a href="https://colorlib.com" target="_blank">
-            Colorlib.com
-          </a>
-          {/* Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. */}
-        </p>
-      </div>
-    </div>
-  </nav>
-  {/* Page Content  */}
-  <div id="content" className="p-4 p-md-5 pt-5"></div>
-</div>
-
-            </div>
-        <div>
-            
-            <h3 >
-                Tableau de borde d'état d'avancement
-            </h3>
           
           <Container  >
       <Navbar expand="lg" variant="light" bg="light"  >
@@ -135,7 +69,7 @@ render(){
         </div>
         </div>
         </div>
-        </div>
+        
     )
 }
     
